ca70c82d1c2fe5edd11e279c4ccad5e215d91639
Oasiris <teracontinens@gmail.com>
2021-12-22 05:08:33 -0500

    Starter code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code:<br>2021🎄
+
+My solutions for [Advent of Code 2021](https://adventofcode.com/).
---
+++ lib/util.ts
@@ -0,0 +1,6 @@
+/**
+ * @returns Whether the given value is both defined and non-null.
+ */
+export function exists<T>(val: T): val is NonNullable<T> {
+    return val !== undefined && val !== null
+}
